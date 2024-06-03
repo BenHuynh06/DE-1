06ebe892332dccf534d45bc5e8b252e68831f228
huy9633 <lamchanhuy9633@gmail.com>
2024-06-03 08:30:49 +0700

    upload q6

--- DE 1 PE/Q6/Q6.cpp
+++ DE 1 PE/Q6/Q6.cpp
@@ -1,29 +1,23 @@
-#include <stdio.h>
-#include <string.h>
-#include <ctype.h>
+#include <bits/stdc++.h>
+using namespace std;
 
-char input(char str[101]);
-char output(char str[101]);
-
-int main () {
-	char str[101];
-	
-	input(str);
-	output(str);
-	
-	return 0;
+void handleStr(string a) {
+    string resuit = "";
+    for (int i = 0; i < a.size(); i++) {
+        if (i % 2 == 0) {
+            resuit += toupper(a[i]);
+        }
+        else {
+            resuit += a[i];
+        }
+    }
+    cout << resuit;
 }
 
-char input(char str[101]) {
-	fgets (str, sizeof(str), stdin);
+int main() {
+    string str;
+    getline(cin, str);
+    handleStr(str);
+    return 0;
 }
 
-char output(char str[101]) {
-	for (int i = 0; str[i] != '\0'; i++) {
-		if(i % 2 == 0) {
-			str[i] = toupper(str[i]);
-		}
-	}
-	printf("%s", str);
-	return 0;
-}
